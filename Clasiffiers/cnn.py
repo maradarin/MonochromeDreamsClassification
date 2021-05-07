e61fbbee432811c199e58a1eb407f94ec2241f9e
Agha Mara <62206596+maradarin@users.noreply.github.com>
2021-05-08 00:12:31 +0300

    Update cnn.py

--- Clasiffiers/cnn.py
+++ Clasiffiers/cnn.py
@@ -15,11 +15,13 @@ import matplotlib.pyplot as plt
 
 plt.style.use('fivethirtyeight')
 
-tf.random.set_seed(2817)
+tf.random.set_seed(8977)
+
+viz = [0 for i in range(11)]
 
 def read_data(name):
-    file_name = name + ".txt"
-    folder_name = name + "/"
+    file_name = "data/" + name + ".txt"
+    folder_name = "data/" + name + "/"
     file = open(file_name, "r")
     lines = file.readlines()
     labels = []
@@ -48,6 +50,24 @@ def read_data(name):
 
     return labels, images
 
+def activation_layers(model, test_images, test_labels, layer):
+    viz = [0 for i in range(11)]
+    for i in range(100):
+        if(viz[int(test_labels[i])] == 0):
+            viz[int(test_labels[i])] = 1
+            image_batch = np.expand_dims(test_images[i], axis=0)
+            layer_outputs = [layer.output for layer in model.layers[:9]]
+            activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+            activations = activation_model.predict(image_batch)
+            first_layer_activation = activations[0]
+            #print(len(first_layer_activation.shape))
+            if(len(first_layer_activation.shape) == 3):
+                plt.matshow(first_layer_activation[0, :, :], cmap='viridis')
+            else:
+                plt.matshow(first_layer_activation[0, :, :, 4], cmap='viridis')
+            plt.title(str(test_labels[i]) + " - " + str(layer))
+            plt.show()
+
 def build_model():
     # Model potrivit pentru crearea unei stive de straturi ce conține strict
     # un strat de input și unul de output
@@ -58,36 +78,48 @@ def build_model():
     # width x height x number of channels = 32 x 32 x 1 (grayscale)
     model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(32, 32, 1),
                      kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+    activation_layers(model, validation_images, validation_labels, "Input layer")
+
     # Strat de pooling, folosit pentru a reduce dimensiunile volumului de output
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    activation_layers(model, validation_images, validation_labels, "First MaxPool layer, size = 2")
 
     # Strat convoluțional, am crescut numărul de filtre așa încât modelul
     # să poată învăța caracteristici mai complexe din setul de date
     model.add(Conv2D(128, (5, 5), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+    activation_layers(model, validation_images, validation_labels, "Conv2D, kernel size = 5")
 
     # Strat de pooling, folosit pentru a reduce dimensiunile volumului de output
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    activation_layers(model, validation_images, validation_labels, "Second MaxPool layer, size = 2")
+
     model.add(Flatten())
+    activation_layers(model, validation_images, validation_labels, "Flatten layer")
 
     # Strat complet conectat (fiecare neuron primeste input de la toți neuronii din
     # stratul precedent)
     model.add(Dense(750, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+    activation_layers(model, validation_images, validation_labels, "Dense layer with 750 neurons")
 
     # Se renunță în mod aleator la 50% dintre caracteristicile învățate până acum
     model.add(Dropout(0.5))
+    activation_layers(model, validation_images, validation_labels, "Dropout layer: 0.5")
 
     # Strat complet conectat (fiecare neuron primeste input de la toți neuronii din
     # stratul precedent)
     model.add(Dense(250, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+    activation_layers(model, validation_images, validation_labels, "Dense layer with 250 neurons")
 
     # Strat de output, imaginile vor fi distribuite în 9 clase
     # Funcția de activare permite o distribuție de probabilități pe baza
     # căreia se va prezice eticheta imaginii
     model.add(Dense(9, activation='softmax'))
+    activation_layers(model, validation_images, validation_labels, "Output layer")
 
     # Funcția de pierdere: specifică problemelor de multi-clasificare (mai mult de 2 etichete)
     # Funcția de optimizare simulează algoritmul de scădere după gradientul stochastic
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    activation_layers(model, validation_images, validation_labels, "After compilation")
 
     return model
 
@@ -131,6 +163,7 @@ def confusion_matrix(y_true, y_pred):
              for column in range(num_classes)
             ]for row in range(num_classes)]
 
+
 train_labels, train_images = read_data("train")
 validation_labels, validation_images = read_data("validation")
 id_images, test_images = read_data("test")
@@ -162,13 +195,3 @@ plot_graph(hist, 'loss')
 fig, ax = plt.subplots(figsize=(10,10))
 sns.heatmap(cf_matrix, linewidths=1, annot=True, ax=ax, fmt='g')
 plt.show()
-
-for i in range(10):
-    image_batch = np.expand_dims(test_images[i], axis=0)
-    layer_outputs = [layer.output for layer in model.layers[:9]]
-    activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
-    activations = activation_model.predict(image_batch)
-    first_layer_activation = activations[0]
-    print(first_layer_activation.shape)
-    plt.matshow(first_layer_activation[0, :, :, 4], cmap='viridis')
-    plt.show()
